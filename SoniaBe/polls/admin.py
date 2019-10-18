0b4c2e723d8907515706d3f9da444e37b4606526
tortueTortue <dxlatortue@gmail.com>
2019-10-17 22:12:25 -0400

    Updated models

--- SoniaBe/polls/admin.py
+++ SoniaBe/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ModuleName,Layout,Module
+
+admin.site.register(ModuleName)
+admin.site.register(Layout)
+admin.site.register(Module)
--- SoniaBe/polls/models.py
+++ SoniaBe/polls/models.py
@@ -2,23 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+
 class ModuleName(models.Model):
     name = models.CharField(max_length=150)
 
-class ModuleList(models.Model):
-    listName = models.ForeignKey(ModuleName, on_delete=models.DO_NOTHING)
 
 class Layout(models.Model):
     name = models.CharField(max_length=50)
-    modules = models.ForeignKey(ModuleList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Module(models.Model):
     name = models.ForeignKey(ModuleName, on_delete=models.DO_NOTHING)
-    belongsToList = models.ForeignKey(ModuleList, on_delete=models.DO_NOTHING)
+    belongsToLayout = models.ForeignKey(Layout, on_delete=models.DO_NOTHING)
     x = models.IntegerField(default=0)
     y = models.IntegerField(default=0)
     h = models.IntegerField(default=0)
     w = models.IntegerField(default=0)
     isResizable = models.BooleanField(default=True)
     isDraggable = models.BooleanField(default=True)
-    isStatic = models.BooleanField(default=False)    
+    isStatic = models.BooleanField(default=False)
